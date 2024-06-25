f13666daa8be74dcf0f381d1a7afe491b804a42a
NIRESH SHRESTHA <dewebkiller@gmail.com>
2024-06-25 22:02:01 +0545

    site completed v1.0

--- about.php
+++ about.php
@@ -1,77 +1,104 @@
-<?php 
+<?php
 /* Template Name: About */
 get_header('inner');
 ?>
 <section class="section-padding section-methodology">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 pull-left">
-                <div class="simple-mage">
-                    <img src="<?php echo bloginfo('template_url'); ?>/images/benifits.png" alt="" />
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 list-style pull-left">
-                <h5 class="btn btn-sm color-primary px-3 hvr-fade bg-primary-shade-1">About us</h5>
-                <h2 class="color-orange">Who we are?</h2>
-                <div class="simnple-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident ,sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam.</p>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <?php
+      if (have_posts()) {
+        while (have_posts()) {
+          the_post(); ?>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 pull-left">
+            <div class="simple-mage">
+              <?php
+              // Must be inside a loop.
 
-        </div>
+              if (has_post_thumbnail()) {
+                the_post_thumbnail();
+              } else {
+                echo '<img src="' . get_bloginfo('stylesheet_directory')
+                  . '/images/benifits.png" />';
+              }
+              ?>
+             
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 list-style pull-left">
+
+
+            <h5 class="btn btn-sm color-primary px-3 hvr-fade bg-primary-shade-1"><?php the_title(); ?></h5>
+        <?php
+          the_content();
+        } // end while
+      } // end if
+        ?>
+          </div>
+
     </div>
+  </div>
 </section>
 <?php get_template_part('includes/happy', 'students'); ?>
 <section class="section-padding section-accordion">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-            <h5 class="btn btn-sm color-primary px-3 hvr-fade bg-primary-shade-1">FAQ</h5>
-                <h2>We want your career to be in a peak</h2>
-                <div class="faq-wrapper">
-                <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h5 class="btn btn-sm color-primary px-3 hvr-fade bg-primary-shade-1">FAQ</h5>
+        <h2>We want your career to be in a peak</h2>
+        <div class="faq-wrapper">
+          <div class="accordion" id="accordionExample">
+            <?php
+            $page_id = 88; // Get the current page ID\
+            if (function_exists('have_rows')) {
+              // Check if there are rows in the 'about_faq' field for the specific page
+              if (have_rows('about_faq', $page_id)) :
+                $counter = 1; // Initialize a counter variable
+                echo '<!-- about_faq exists -->';
+                while (have_rows('about_faq', $page_id)) : the_row();
+                  $title = get_sub_field('about_faq_title', $page_id);
+                  $description = get_sub_field('about_faq_description', $page_id);
+
+                  if (!$title || !$description) {
+                    echo '<!-- Missing title or description for FAQ item ' . $counter . ' -->';
+                    continue;
+                  }
+
+                  // Determine the class for the collapse element
+                  $collapse_class = ($counter == 1) ? 'accordion-collapse collapse show acollapse' : 'accordion-collapse collapse acollapse';
+                  // Determine the class for the button
+                  $button_class = ($counter == 1) ? 'accordion-button' : 'accordion-button collapsed';
+                  // Debugging statement
+                  echo '<!-- FAQ item ' . $counter . ' -->';
+            ?>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading<?php echo $counter; ?>">
+                      <button class="<?php echo $button_class; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $counter; ?>" aria-expanded="<?php echo ($counter == 1) ? 'true' : 'false'; ?>" aria-controls="collapse<?php echo $counter; ?>">
+                        <?php echo $title; ?>
+                      </button>
+                    </h2>
+                    <div id="collapse<?php echo $counter; ?>" class="<?php echo $collapse_class; ?>" aria-labelledby="heading<?php echo $counter; ?>" data-bs-parent="#accordionExample">
+                      <div class="accordion-body">
+                        <?php echo $description; ?>
+                      </div>
+                    </div>
+                  </div>
+            <?php
+                  $counter++; // Increment the counter
+                endwhile;
+              else :
+                echo '<!-- No FAQs found in about_faq -->';
+                echo '<p>No FAQs found.</p>';
+              endif;
+            } else {
+              echo '<!-- ACF function have_rows not found -->';
+            }
+            ?>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </section>
+
 <?php get_template_part('includes/tools', 'marquee'); ?>
 <?php get_footer();
--- single-dwk_courses.php
+++ single-dwk_courses.php
@@ -33,36 +33,36 @@
                 <h5 class="btn btn-sm color-primary px-3 hvr-fade bg-primary-shade-1">FAQ</h5>
                 <h2>We want your career to be in a peak</h2>
                 <div class="faq-wrapper">
-                <div class="accordion" id="accordionExample">
-    <?php
-    if (have_rows('courses_faq')) :
-        $counter = 1; // Initialize a counter variable
-        while (have_rows('courses_faq')) : the_row();
-            // Determine the class for the collapse element
-            $collapse_class = ($counter == 1) ? 'accordion-collapse collapse show acollapse' : 'accordion-collapse collapse acollapse';
-            // Determine the class for the button
-            $button_class = ($counter == 1) ? 'accordion-button' : 'accordion-button collapsed';
-    ?>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="heading<?php echo $counter; ?>">
-                <button class="<?php echo $button_class; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $counter; ?>" aria-expanded="<?php echo ($counter == 1) ? 'true' : 'false'; ?>" aria-controls="collapse<?php echo $counter; ?>">
-                    <?php echo get_sub_field('faq_title'); ?>
-                </button>
-            </h2>
-            <div id="collapse<?php echo $counter; ?>" class="<?php echo $collapse_class; ?>" aria-labelledby="heading<?php echo $counter; ?>" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <?php echo get_sub_field('faq_description'); ?>
-                </div>
-            </div>
-        </div>
-    <?php
-        $counter++; // Increment the counter
-        endwhile;
-    else :
-        echo '<p>No FAQs found.</p>';
-    endif;
-    ?>
-</div>
+                    <div class="accordion" id="accordionExample">
+                        <?php
+                        if (have_rows('courses_faq')) :
+                            $counter = 1; // Initialize a counter variable
+                            while (have_rows('courses_faq')) : the_row();
+                                // Determine the class for the collapse element
+                                $collapse_class = ($counter == 1) ? 'accordion-collapse collapse show acollapse' : 'accordion-collapse collapse acollapse';
+                                // Determine the class for the button
+                                $button_class = ($counter == 1) ? 'accordion-button' : 'accordion-button collapsed';
+                        ?>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="heading<?php echo $counter; ?>">
+                                        <button class="<?php echo $button_class; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $counter; ?>" aria-expanded="<?php echo ($counter == 1) ? 'true' : 'false'; ?>" aria-controls="collapse<?php echo $counter; ?>">
+                                            <?php echo get_sub_field('faq_title'); ?>
+                                        </button>
+                                    </h2>
+                                    <div id="collapse<?php echo $counter; ?>" class="<?php echo $collapse_class; ?>" aria-labelledby="heading<?php echo $counter; ?>" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <?php echo get_sub_field('faq_description'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                        <?php
+                                $counter++; // Increment the counter
+                            endwhile;
+                        else :
+                            echo '<p>No FAQs found.</p>';
+                        endif;
+                        ?>
+                    </div>
 
 
 
